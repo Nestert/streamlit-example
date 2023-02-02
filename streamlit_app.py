24bebf50c054b12f564d7b0ccc4d7460d71f4dd5
Valentin <kindnessbel@mail.ru>
2023-02-02 23:11:16 +0300

    update path

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import networkx as nx
 import streamlit as st
 
 # Load the NER model
-nlp = spacy.load('model\ru_core_news_md-3.5.0')
+nlp = spacy.load(r'model\ru_core_news_md-3.5.0')
 
 # Function to process the text and extract entities
 def process_text(text):
